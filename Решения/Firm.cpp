b53de455f6cf1ad9ce0afdfe456cf3e4bbef7878
Scorpion333 <pskorcheliev@gmail.com>
2017-03-01 23:59:19 +0200

    week 2 sol part 1

--- Решения/Firm.cpp
+++ Решения/Firm.cpp
@@ -2,81 +2,146 @@
 #include<cstring>
 using namespace std;
 
-struct Programmer
-{
+class Programmer {
+private:
 	char name[20];
 	unsigned languages;
 	unsigned salary;
+public:
+	// SETTERS
+	void set_name(char* _name) {
+		strcpy(name, _name);
+	}
+	void set_languages(unsigned _languages) {
+		languages = _languages;
+	}
+	void set_salary(unsigned _salary) {
+		salary = _salary;
+	}
 
-	void read()
-	{
+	// GETTERS
+	char* get_name() {
+		char* result;
+		result = new char[strlen(name) + 1];
+		strcpy(result, name);
+		return result;
+	}
+	unsigned get_languages() {
+		return languages;
+	}
+	unsigned get_salary() {
+		return salary;
+	}
+
+	// INPUT AND OUTPUT
+	void read() {
 		cout << "name: ";      cin >> name;
 		cout << "languages: "; cin >> languages;
 		cout << "salary: ";    cin >> salary;
 	}
-	void print()
-	{
-		cout << "name: "      << name      << endl
+	void print() {
+		cout << "name: " << name << endl
 			 << "languages: " << languages << endl
-			 << "salary: "    << salary    << endl;
+			 << "salary: " << salary << endl;
 	}
-	bool correctData()
-	{
-		return languages > 0 && salary > 0;
+	
+	// OPERATORS
+	bool operator < (Programmer other) {
+		return languages < other.languages;
 	}
-	bool knowsMoreThan(Programmer other)
-	{
+	bool operator > (Programmer other) {
+		return languages > other.languages;
+	}
+	bool knows_more_than(Programmer other) {
 		return languages > other.languages;
 	}
 };
 
-class Firm
-{
+class Firm {
 private:
 	char* name;
 	Programmer* employees;
 	unsigned employees_number;
 	unsigned capacity;
 public:
-	void initialize(char* _name, unsigned _capacity)
-	{
+	void initialize(char* _name, unsigned _capacity) {
 		name = new char[strlen(_name) + 1];
 		strcpy(name, _name);
 		capacity = _capacity;
 		employees = new Programmer[capacity];
 		employees_number = 0;
 	}
-	void hire(Programmer junior)
-	{
-		if (employees_number < capacity)
-		{
+	void clear_memory() {
+		delete[] name;
+		delete[] employees;
+	}
+
+	// SETTERS AND GETTERS
+	void set_name(char* _name) {
+		delete[] name;
+		name = new char[strlen(_name) + 1];
+		strcpy(name, _name);
+	}
+	char* get_name() {
+		char* result = new char[strlen(name) + 1];
+		strcpy(result, name);
+		return result;
+	}
+	unsigned get_employees_number() {
+		return employees_number;
+	}
+	unsigned get_capacity() {
+		return capacity;
+	}
+	Programmer get_employee(unsigned i) {
+		return employees[i];
+	}
+
+	// METHODS FOR THE EMPLOYEES
+	void hire(Programmer junior) {
+		if (employees_number < capacity) {
 			employees[employees_number] = junior;
 			employees_number++;
 		}
 	}
-	void fire_last(unsigned n)
-	{
+	void fire_last(unsigned n) {
 		employees_number -= n;
 	}
-	void print_employees()
-	{
+	void print_employees() {
 		for (unsigned i = 0; i < employees_number; i++)
 			employees[i].print();
 	}
-	char* lucky_name()
-	{
-		char* result = new char[strlen(employees[0].name) + 1];
-		strcpy(result, employees[0].name);
+	char* lucky_name() {
+		char* result = new char[strlen(employees[0].get_name()) + 1];
+		strcpy(result, employees[0].get_name());
 		return result;
 	}
-	void clear_memory()
-	{
-		delete[] name;
-		delete[] employees;
+
+	// OPERATORS
+	Firm operator + (Firm other) {
+		Firm sum;
+		sum.initialize(name, capacity + other.capacity);
+		for (unsigned i = 0; i < employees_number; i++)
+			sum.hire(employees[i]);
+		for (unsigned i = 0; i < other.employees_number; i++)
+			sum.hire(other.employees[i]);
+		return sum;
+	}
+	unsigned salaries_sum() {
+		unsigned sum = 0;
+		for (unsigned i = 0; i < employees_number; i++) {
+			sum += employees[i].get_salary();
+		}
+		return sum;
+	}
+	bool operator > (Firm other) {
+		return salaries_sum() > other.salaries_sum();
+	}
+	bool operator < (Firm other) {
+		return salaries_sum() < other.salaries_sum();
 	}
 };
 
-int main()
-{
+int main() {
 	return 0;
 }
