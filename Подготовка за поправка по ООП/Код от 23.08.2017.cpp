1b882e055756b5b71d612a922ae108bc5eb10b07
Petar Skorcheliev <pskorcheliev@gmail.com>
2017-08-24 16:59:43 +0300

    fixed >>

--- Подготовка за поправка по ООП/Код от 23.08.2017.cpp
+++ Подготовка за поправка по ООП/Код от 23.08.2017.cpp
@@ -1,6 +1,3 @@
-
-// DEBUG NEEDED!
-
 #include<iostream>
 #include<cstring>
 #include<fstream>
@@ -23,7 +20,7 @@ public:
 		name = new char[strlen(other.name) + 1];
 		strcpy(name, other.name);
 	}
-	Student& operator = (const Student& other) {	
+	Student& operator = (const Student& other) {
 		if (this != &other) {
 			fn = other.fn;
 			grade = other.grade;
@@ -37,8 +34,6 @@ public:
 		delete[] name;
 	}
 
-	// const
-
 	int getFn() const {
 		return fn;
 	}
@@ -50,7 +45,7 @@ public:
 	}
 
 	void setFn(int _fn) {
-		if(_fn >= 100000 && _fn <= 999999)
+		if (_fn >= 100000 && _fn <= 999999)
 			fn = _fn;
 	}
 	void setGrade(double _grade) {
@@ -66,15 +61,15 @@ public:
 };
 
 istream& operator >> (istream& in, Student& st) {
-	
 	int capacity = 4;
 	char* helper = new char[capacity + 1];
 	int i = 0;
 
 	while (in.peek() != ' ' && in.peek() != '\n' && !in.eof()) {
 		if (i == capacity) {
-			char* temp = new char[2 * capacity + 1];
+			helper[i] = '\0';	// Strcpy needs this!
 			capacity *= 2;
+			char* temp = new char[capacity + 1];
 			strcpy(temp, helper);
 			delete[] helper;
 			helper = temp;
@@ -83,10 +78,13 @@ istream& operator >> (istream& in, Student& st) {
 		i++;
 	}
 	helper[i] = '\0';
-	in.get();
 
 	st.setName(helper);
+	delete[] helper;			// No memory leaks!
+
 	in >> st.fn >> st.grade;
+
+	in.get();					// Newline character after the grade also caused trouble.
 	return in;
 }
 
@@ -95,62 +93,4 @@ ostream& operator << (ostream& out, const Student& st) {
 		<< st.getFn() << "\n"
 		<< st.getGrade();
 	return out;
-}
-
-int main() {
-	int capacity = 4;
-	Student* students = new Student[capacity];
-	int i = 0;
-
-	ifstream in("students1.txt");
-	
-	while (!in.eof()) {
-		if (i == capacity) {
-			capacity *= 2;
-			Student* temp = new Student[capacity];
-			for (int j = 0; j < i; j++) {
-				temp[j] = students[j];
-			}
-			delete[] students;
-			students = temp;
-		}
-		in >> students[i];
-		i++;
-	}
-	in.close();		// Optional
-
-	ofstream out("students2.txt");
-
-	int* seenFn = new int[i];
-	int seenFnSize = 0;
-
-	for (int j = 0; j < i; j++) {
-		
-		bool fnIsSeen = false;
-		
-		for (int k = 0; k < seenFnSize; k++) {
-			if (seenFn[k] == students[j].getFn()) {
-				fnIsSeen = true;
-			}
-		}
-		
-		if (!fnIsSeen) {
-			double grade = 0;
-			int count = 0;
-			for (int k = 0; k < i; k++) {
-				if (students[k].getFn() == students[j].getFn()) {
-					grade += students[k].getGrade();
-					count++;
-				}
-			}
-			grade /= count;
-			students[j].setGrade(grade);
-
-			seenFn[seenFnSize] = students[j].getFn();
-			seenFnSize++;
-
-			out << students[j] << '\n';
-		}
-	}
-	return 0;
 }
